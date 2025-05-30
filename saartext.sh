c82769f6f104f1022e02365858518ed7eb1f3204
holm / Christian Müller <holm+termux@mueller.network>
2025-05-31 01:40:45 +0200

    Interaktiver Modus

--- saartext.sh
+++ saartext.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-page=$1
+process() {
+  curl -s "https://www.saartext.de/$page" | \
+    sed -n '/<pre/,/<\/pre>/p' | \
+    sed s/"                    <pre class=\"saartext_page\">"/"<pre>"/ | \
+    sed s/"<\/pre>"// | \
+    sed s/"                                    "// | \
+    pandoc --from html --to markdown_strict
+}
 
-if [ -z "$1" ]
-  then
-    page=110
-fi
+main() {
+  local page="${1:-110}"
+  while true; do
+        process "$page"
 
-curl -s "https://www.saartext.de/$page" | sed -n '/<pre/,/<\/pre>/p' | sed s/"                    <pre class=\"saartext_page\">"/"<pre>"/ | sed s/"<\/pre>"// | sed s/"                                    "// | pandoc --from html --to markdown_strict
+        # Benutzereingabe abfragen
+        read -p "Neue Seite eingeben (oder 'q' zum Beenden): " input
+
+        # Überprüfe die Eingabe
+        if [[ "$input" == "q" || "$input" == "Q" ]]; then
+            echo "Programm wird beendet."
+            break
+        elif [[ "$input" =~ ^[0-9]+$ ]]; then
+            page="$input"
+        else
+            echo "Ungültige Eingabe. Bitte eine Zahl eingeben oder 'q' zum Beenden."
+        fi
+    done
+}
+
+main "$@"
+
