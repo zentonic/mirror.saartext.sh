393e986b19f098d26ea467303a32e7a8561824f7
holm / Christian Müller <holm+termux@mueller.network>
2025-07-09 04:04:45 +0200

    Nächste Seite - per wAsD/HjkL/BN Added #2

--- saartext.sh
+++ saartext.sh
@@ -1,37 +1,52 @@
 #!/bin/bash
 
-space1="                   " 
+space1="                   "
 space2="                                    "
 
 process() {
-  curl -s "https://www.saartext.de/$page" | \
-    sed -n '/<pre/,/<\/pre>/p' | \
-    sed s/$space1\<pre class=\"saartext_page\"\>/\<pre\>/ | \
-    sed s/"<\/pre>"// | \
-    sed s/$space2// | \
+  curl -s "https://www.saartext.de/$page" |
+    sed -n '/<pre/,/<\/pre>/p' |
+    sed s/"$space1\<pre class=\"saartext_page\"\>"/\<pre\>/ |
+    sed s/"<\/pre>"// |
+    sed s/"$space2"// |
     pandoc --from html --to markdown_strict
 }
 
 main() {
   local page="${1:-110}"
   while true; do
-        process "$page"
+    process "$page"
 
-        # Benutzereingabe abfragen
-        read -p "Neue Seite eingeben (oder 'q' zum Beenden): " input
-        echo
+    # Benutzereingabe abfragen
+    read -p "Neue Seite eingeben (oder 'q' zum Beenden): " input
+    echo
 
-        # Überprüfe die Eingabe
-        if [[ "$input" == "q" || "$input" == "Q" ]]; then
-            echo "Programm wird beendet."
-            break
-        elif [[ "$input" =~ ^[0-9]+$ ]]; then
-            page="$input"
-        else
-            page=110 
-        fi
-    done
+    # Überprüfe die Eingabe
+    if [[ "$input" == "q" || "$input" == "Q" ]]; then
+      echo "Programm wird beendet."
+      break
+    elif [[ 
+      "$input" == "n" ||
+      "$input" == "N" ||
+      "$input" == "l" ||
+      "$input" == "L" ||
+      "$input" == "d" ||
+      "$input" == "D" ]]; then
+      page=$(($page + 1))
+    elif [[ 
+      "$input" == "b" ||
+      "$input" == "B" ||
+      "$input" == "h" ||
+      "$input" == "H" ||
+      "$input" == "a" ||
+      "$input" == "A" ]]; then
+      page=$(($page - 1))
+    elif [[ "$input" =~ ^[0-9]+$ ]]; then
+      page="$input"
+    else
+      page=110
+    fi
+  done
 }
 
 main "$@"
-
