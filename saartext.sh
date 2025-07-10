d7808ef84a1ba93c67fd9396697847c8051b6246
holm / Christian Müller <holm+termux@mueller.network>
2025-07-10 08:03:26 +0200

    Colors

--- saartext.sh
+++ saartext.sh
@@ -3,6 +3,7 @@
 process() {
   clear
   if [[ "$page" == "help" ]]; then
+    echo -en "${C2}"
     cat <<EOF
 
       saartext.sh
@@ -18,24 +19,43 @@ process() {
       n,l,d     + Eine Seite vor
 
 EOF
+    echo -en "${R}"
   else
+    echo -en "${C4}"
     curl -s "https://www.saartext.de/$page" |
       sed -e s/"saartext_page\">"/"\\n<SAARTEXT_START>"/ |
       sed -e s/"<\/pre>"/"<SAARTEXT_END>"/ |
       sed -nr '/SAARTEXT_START/,/SAARTEXT_END/p' |
       sed -e 's/<[^>]*>//g' |
       uniq
+    echo -en "${R}"
   fi
 }
 
 main() {
+  R="\e[0m"   # reset
+  B="\e[1m"   # bold
+  C1="\e[31m" # rot
+  C2="\e[32m" # grün
+  C3="\e[33m" # gelb
+  C4="\e[34m" # blau
+  C5="\e[35m" # lila
+  C6="\e[36m" # hellblau
+
   local page="${1:-110}"
   while true; do
     process "$page" && [ -n "$1" ] && exit 0
 
     # Benutzereingabe abfragen
-    read -p "[Q]uit hel[P] [b]$((page - 1)) [n]$((page + 1)) $page Page " input
-    echo
+    # echo -e "0${BC1}1${BC2}2${C3}3${C4}4${C5}5${C6}6${R} "
+
+    echo -en "${C4}Q${C5}uit${R} "
+    echo -en "${C5}hel${C4}P${R} "
+    echo -en "                   "
+    echo -en "${B}${C1}-${C6}$page${B}${C2}+${R} "
+    echo -en " ${B}${C6}#${R}${C2}"
+    read -n3 input
+    echo -en "${R}"
 
     # Überprüfe die Eingabe
     case "$input" in
